62b8743d890fd8ec93c6227fc0ad9b79bcf118b6
kkb75281 <kkb75281@gmail.com>
2022-10-11 14:29:27 +0900

    221011

---
+++ sabbun/connect/connect.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "sabbun.dothome.co.kr";
+    $user = "sabbun";
+    $pass = "sopum1004!";
+    $db = "sabbun";
+    $connect = new mysqli($host, $user, $pass, $db);
+    $connect -> set_charset("utf8");
+?>
