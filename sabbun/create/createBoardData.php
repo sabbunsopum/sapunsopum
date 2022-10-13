1229accb330ebe6baf51c62660723d73bc4c719a
younghwan12 <109262134+younghwan12@users.noreply.github.com>
2022-10-13 17:12:37 +0900

    221013

--- sabbun/create/createBoardData.php
+++ sabbun/create/createBoardData.php
@@ -1,1 +1,8 @@
-
+<?php
+    include "../connect/connect.php";
+    for($i=1; $i<=500; $i++){
+        $regTime = time();
+        $sql = "INSERT INTO myBoard(myMemberID, boardTitle, boardContents, boardView, boardLike, regTime) VALUES('2', '게시판 타이틀${i}입니다.', '게시판 컨텐츠${i}입니다.', '1', '1 , '$regTime')";
+        $connect -> query($sql);
+    }
+?>
