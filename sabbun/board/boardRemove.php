63b294d4ac4f863220e8501f9018246a52fde4cb
sabbunsopum <115064300+sabbunsopum@users.noreply.github.com>
2022-10-18 19:06:29 +0900

    123
    1234

--- sabbun/board/boardRemove.php
+++ sabbun/board/boardRemove.php
@@ -8,40 +8,22 @@
     $myMemberID = $_SESSION['myMemberID'];
 
 
-
-
-
-
-
-
     $sql = "SELECT myMemberID FROM myBoard WHERE myBoardID = {$myBoardID}";
     $result2 = $connect -> query($sql);
     $myBoardMember = $result2 -> fetch_array(MYSQLI_ASSOC);
-
-
 
 
     $sql = "SELECT myMemberID FROM myBMember WHERE myMemberID = {$myMemberID}";
     $result = $connect -> query($sql);
     $memberInfo = $result -> fetch_array(MYSQLI_ASSOC);
 
-
-
+    
     if($memberInfo['myMemberID'] === $myMemberID && $myBoardMember['myMemberID'] === $myMemberID ){
         $sql = "DELETE FROM myBoard WHERE myBoardID = {$myBoardID}";
         $connect -> query($sql);
     } else {
-        echo "<script>alert('내가 작성한 글이 아닙니다. 다시 한번 확인해주세요 !')</script>";
-    }
-
-
-
-
-
-
-
-
-    
+        echo "<script>alert('내가 작성한 글이 아닙니다. 다시 한번 확인해주세요!')</script>";
+    } 
 ?>
 
 <script>
