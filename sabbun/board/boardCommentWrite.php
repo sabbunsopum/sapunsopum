9306a26842e9ddf0decd1bf7e49e8ee01efc5c6c
sabbunsopum <sabbunsopum@gmail.com>
2022-10-25 19:17:07 +0900

    comment
    comment

---
+++ sabbun/board/boardCommentWrite.php
@@ -0,0 +1,14 @@
+<?php
+    include "../connect/connect.php";
+
+    $myBoardID = $_POST['boardID'];
+    $commentName = $_POST['name'];
+    $commentPass = $_POST['pass'];
+    $commentMsg = $_POST['msg'];
+    $regTime = time();
+
+    $sql = "INSERT INTO myComment(myMemberID, myBoardID, commentName, commentMsg, commentPass, commentDelete, regTime) VALUES ('1', '$myBoardID', '$commentName', '$commentMsg', '$commentPass', '0','$regTime')";
+    $result = $connect -> query($sql);
+
+    echo json_encode(array("info" => $myBoardID));
+?>
