a336ce4a0c91ddd1390849bb732b5e7b4b27998e
kkb75281 <kkb75281@gmail.com>
2022-10-25 15:26:43 +0900

    221025

--- sabbun/include/link.php
+++ sabbun/include/link.php
@@ -4,11 +4,6 @@
     <link rel="stylesheet" href="../html/assets/css/reset.css">
     
     <link rel="stylesheet" href="../html/assets/css/header.css">
-    <link rel="stylesheet" href="../html/assets/css/slider.css">
-    <link rel="stylesheet" href="../html/assets/css/swiper.css">
-    <link rel="stylesheet" href="../html/assets/css/imgText.css">
-    <link rel="stylesheet" href="../html/assets/css/cardSlider.css">
-    <link rel="stylesheet" href="../html/assets/css/textType.css">
-    <link rel="stylesheet" href="../html/assets/css/banner.css">
-    <link rel="stylesheet" href="../html/assets/css/textType2.css">
+    <link rel="stylesheet" href="../html/assets/css/main.css">
+    <link rel="stylesheet" href="../html/assets/js/test/swiper.min.css">
     <link rel="stylesheet" href="../html/assets/css/footer.css">
