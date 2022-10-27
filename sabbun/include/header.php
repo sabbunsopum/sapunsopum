29161f6405909752b50c6f99ff935bc28cbc140e
sabbunsopum <sabbunsopum@gmail.com>
2022-10-27 17:18:16 +0900

    header
    asd

--- sabbun/include/header.php
+++ sabbun/include/header.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>헤더영역</title>
-    
+
     <link rel="stylesheet" href="../html/assets/css/fonts.css">
     <link rel="stylesheet" href="../html/assets/css/header.css">
     <link rel="stylesheet" href="../html/assets/css/common.css">
     <link rel="stylesheet" href="../html/assets/css/reset.css">
 </head>
+
 <body>
     <header id="headerType" class="header__wrap nanum">
         <div class="header__inner">
@@ -22,7 +24,7 @@
             <nav class="header__menu clearfix">
                 <ul>
                     <li><a href="#sliderType">사뿐소품이란?</a></li>
-                    <li><a href="../list/sopumList.php">소품샵 리스트</a></li>
+                    <li><a href="../sopumList/sopumList.php">소품샵 리스트</a></li>
                     <li><a href="../board/board.php">커뮤니티</a></li>
                     <li><a href="../mypage/myPage.php">마이페이지</a></li>
                     <li><a href="../service/faq.php">고객센터</a></li>
@@ -30,13 +32,13 @@
             </nav>
             <div class="header__member clearfix">
                 <ul>
-<?php if( isset($_SESSION['myMemberID'])){ ?>
-        <li><a href="#" class="black"><?=$_SESSION['youName']?>님 환영합니다:)</a></li>
-        <li><a href="../login/logout.php">로그아웃</a></li>
-<?php }else {   ?>
-        <li><a href="../login/login.php">로그인</a></li>
-        <li><a href="../join/join1.php">회원가입</a></li>
-<?php  }   ?>
+                    <?php if( isset($_SESSION['myMemberID'])){ ?>
+                    <li><a href="../mypage/myPage.php" class="black"><?=$_SESSION['youName']?>님 환영합니다:)</a></li>
+                    <li><a href="../login/logout.php">로그아웃</a></li>
+                    <?php }else {   ?>
+                    <li><a href="../login/login.php">로그인</a></li>
+                    <li><a href="../join/join1.php">회원가입</a></li>
+                    <?php  }   ?>
                     <!-- <li><a href="login.html">로그인</a></li>
                     <li><a href="join1.html">회원가입</a></li> -->
                 </ul>
@@ -45,7 +47,8 @@
                 <form action="headerSearch.php" name="headerSearch" method="get">
                     <fieldset>
                         <legend>검색 영역</legend>
-                        <input type="search" name="searchKeyword" id="searchKeyword" placeholder="검색어를 입력하세요 !" aria-label="search" required>
+                        <input type="search" name="searchKeyword" id="searchKeyword" placeholder="검색어를 입력하세요 !"
+                            aria-label="search" required>
                         <button type="submit" class="searchBtn">
                             <img src="../html/assets/img/search_icon.svg" alt="검색버튼">
                         </button>
@@ -61,4 +64,5 @@
     </header>
     <!-- // headerType -->
 </body>
+
 </html>
