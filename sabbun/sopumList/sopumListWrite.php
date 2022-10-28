52ca6589a4e682f35a70483b6b0cb9ed5de3feac
sabbunsopum <sabbunsopum@gmail.com>
2022-10-28 13:08:56 +0900

    asd
    asd

--- sabbun/sopumList/sopumListWrite.php
+++ sabbun/sopumList/sopumListWrite.php
@@ -5,6 +5,16 @@
 ?>
 <!DOCTYPE html>
 <html lang="ko">
+<style>
+.img__button input[type="file"] {
+    position: absolute;
+    width: 0;
+    height: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 0;
+}
+</style>
 
 <head>
     <meta charset="UTF-8">
@@ -51,68 +61,75 @@
                     <p>다양한 소품샵을 사뿐소품에서 확인해보세요!</p>
                 </div>
                 <!-- 소품샵 리스트 게시판 -->
-
-                <div class="post__box">
-                    <div class="postWrite">
-                        <div class="post__info">
-                            <div class="img">
-                                <div class="img__button">
-                                    <label for="file">
-                                        <div class="btn-upload">
+                <form action="sopumListWriteSave.php" name="sopumListWrite" method="post" enctype="multipart/form-data">
+                    <fieldset>
+                        <div class="post__box">
+                            <div class="postWrite">
+                                <div class="post__info">
+                                    <div class="img">
+                                        <div class="img__button">
+                                            <label for="sopumListFile">
+                                                <div class="btn-upload">
+                                                </div>
+                                                <p>사진을 업로드해주세요!</p>
+                                            </label>
+                                            <input type="file" name="sopumListFile" id="sopumListFile"
+                                                accept=".jpg, .jpeg, .png, .gif">
                                         </div>
-                                        <p>사진을 업로드해주세요!</p>
-                                    </label>
-                                    <input type="file" name="file" id="file">
-                                </div>
 
-                            </div>
-                            <div class="shop__info">
+                                    </div>
+                                    <div class="shop__info">
 
-                                <div>
-                                    <span>업체명</span><input type="text" placeholder="소품샵 이름을 입력해주세요">
-                                </div>
-                                <div>
-                                    <span>영업시간</span><input class="time" type="text" placeholder="영업일/시간을 입력하세요">
-                                </div>
-                                <div>
-                                    <span>연락처</span><input class="phone" type="text" placeholder="소품샵 연락처를 입력하세요">
-                                </div>
-                                <div>
-                                    <span>상품종류</span><input type="text" placeholder="판매하는 상품 종류를 입력해주세요">
-                                </div>
-                                <div>
-                                    <span>위치</span><input type="text" placeholder="소품샵 주소를 입력하세요">
-                                </div>
+                                        <div>
+                                            <span>업체명</span><input type="text" placeholder="소품샵 이름을 입력해주세요">
+                                        </div>
+                                        <div>
+                                            <span>영업시간</span><input class="time" type="text"
+                                                placeholder="영업일/시간을 입력하세요">
+                                        </div>
+                                        <div>
+                                            <span>연락처</span><input class="phone" type="text"
+                                                placeholder="소품샵 연락처를 입력하세요">
+                                        </div>
+                                        <div>
+                                            <span>상품종류</span><input type="text" placeholder="판매하는 상품 종류를 입력해주세요">
+                                        </div>
+                                        <div>
+                                            <span>위치</span><input type="text" placeholder="소품샵 주소를 입력하세요">
+                                        </div>
 
-                                <div class="shop__map">
-                                    <div id="map" style="width:420px;height:230px;margin-left: 50px;"></div>
-                                    <script type="text/javascript"
-                                        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=9d3637cebd20b21457c4e0c648d59d32">
-                                    </script>
-                                    <script>
-                                    var container = document.getElementById('map');
-                                    var options = {
-                                        center: new kakao.maps.LatLng(33.450701, 126.570667),
-                                        level: 3
-                                    };
+                                        <div class="shop__map">
+                                            <div id="map" style="width:420px;height:230px;margin-left: 50px;"></div>
+                                            <script type="text/javascript"
+                                                src="//dapi.kakao.com/v2/maps/sdk.js?appkey=9d3637cebd20b21457c4e0c648d59d32">
+                                            </script>
+                                            <script>
+                                            var container = document.getElementById('map');
+                                            var options = {
+                                                center: new kakao.maps.LatLng(33.450701, 126.570667),
+                                                level: 3
+                                            };
 
-                                    var map = new kakao.maps.Map(container, options);
-                                    </script>
+                                            var map = new kakao.maps.Map(container, options);
+                                            </script>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <!-- <div class="post__share ir">공유</div>
+                                <!-- <div class="post__share ir">공유</div>
                         <div class="post__heart ir">좋아요</div> -->
-                        <div class="tag">
-                            <span class="tag__title">키워드 태그</span>
-                            <input type="text" id="tag" placeholder="태그를 입력해주세요">
+                                <div class="tag">
+                                    <span class="tag__title">키워드 태그</span>
+                                    <input type="text" id="tag" placeholder="태그를 입력해주세요">
+                                </div>
+                            </div>
+                            <div class="container">
+                                <button type="submit" class="btn">글 작성하기</button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="container">
-                        <a href="sopumList.html"><button type="submit" class="btn">글 작성하기</button></a>
-                    </div>
-                </div>
-                <!-- //소품샵 게시판 -->
+                        <!-- //소품샵 게시판 -->
+                    </fieldset>
+                </form>
+
 
 
             </div>
