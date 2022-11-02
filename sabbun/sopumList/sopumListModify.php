fa968d2211b0c49f812531883b5f7cda33e7300f
sabbunsopum <115064300+sabbunsopum@users.noreply.github.com>
2022-11-02 21:19:06 +0900

    asd
    asd

--- sabbun/sopumList/sopumListModify.php
+++ sabbun/sopumList/sopumListModify.php
@@ -3,6 +3,19 @@
     include "../connect/session.php";
     include "../connect/sessionCheck.php";
 
+
+    $myMemberID = $_SESSION['myMemberID'];
+
+    // $mySql = "SELECT * FROM myBMember";
+    // $myResult = $connect -> query($mySql);
+    // $myInfo = $myResult -> fetch_array(MYSQLI_ASSOC);
+
+
+    $shopListID = $_GET['shopListID'];
+    $sql = "SELECT * FROM sopumShopList WHERE shopListID = {$shopListID}";
+    $result = $connect -> query($sql);
+    $info = $result -> fetch_array(MYSQLI_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html lang="ko">
@@ -52,78 +65,90 @@
                     <p>다양한 소품샵을 사뿐소품에서 확인해보세요!</p>
                 </div>
                 <!-- 소품샵 리스트 게시판 -->
-                <div class="post__box">
-                    <div class="postWrite">
-                        <div class="post__info">
-                            <div class="img">
-                                <img src="../html/assets/img/sopumListView_bg01.jpg" alt="">
+                <form action="sopumListModifySave.php" name="modify" method="post" enctype="multipart/form-data"
+                    onsubmit="return modifyChecks()">
+                    <fieldset>
+                        <div class="post__box">
+                            <div class="postWrite">
+                                <div class="post__info">
+                                    <div class="img">
+                                        <img src="img/<?=$info['shopImgFile']?>" alt="">
+                                        <input type="file" name="shopProfile" id="shopProfile"
+                                            accept=".jpg, .jpeg, .png, .gif">
+                                    </div>
+                                    <div class="shop__info">
 
-                            </div>
-                            <div class="shop__info">
+                                        <div>
+                                            <span>업체명</span><input type="text" value=<?=$info['shopName']?>
+                                                placeholder="소품샵 이름을 입력해주세요" name="shopName" id="shopName">
+                                        </div>
+                                        <div>
+                                            <span>소품샵 소개</span><input type="text" value=<?=$info['shopListContents']?>
+                                                name="shopListContents" id="shopListContents"
+                                                placeholder="간단한 소품샵 소개를 입력 해 주세요">
+                                        </div>
+                                        <div>
+                                            <span>영업시간</span><input type="text" value=<?=$info['shopHours']?>
+                                                placeholder="영업일/시간을 입력하세요" name="shopHours" id="shopHours">
 
-                                <div>
-                                    <span>업체명</span><input type="text" value="소르르" placeholder="소품샵 이름을 입력해주세요">
-                                </div>
-                                <div>
-                                    <span>영업시간</span><input type="text" value="화, 수, 목, 금, 토 / 9:00 AM ~ 18:00 PM"
-                                        placeholder="영업일/시간을 입력하세요">
+                                        </div>
+                                        <div>
+                                            <span>연락처</span><input type="text" value=<?=$info['shopNum']?>
+                                                placeholder="소품샵 연락처를 입력하세요" name="shopNum" id="shopNum">
+                                        </div>
+                                        <div>
+                                            <span>상품종류</span><input type="text" value=<?=$info['goodsList']?>
+                                                placeholder="판매하는 상품 종류를 입력해주세요" name="goodsList" id="goodsList">
+                                        </div>
+                                        <div>
+                                            <span>위치</span>
+                                            <input type="text" value=<?=$info['shopAdress']?>
+                                                placeholder="소품샵 주소를 입력하세요" name="shopAdress" id="shopAdress">
+                                        </div>
 
-                                </div>
-                                <div>
-                                    <span>연락처</span><input type="text" value="010 - 1234 - 5678 / soruru@abc.com"
-                                        placeholder="소품샵 연락처를 입력하세요">
-                                </div>
-                                <div>
-                                    <span>상품종류</span><input type="text" value="패브릭, 제로웨이스트, 인테리어"
-                                        placeholder="판매하는 상품 종류를 입력해주세요">
-                                </div>
-                                <div>
-                                    <span>위치</span>
-                                    <input type="text" value="제주특별자치도 제주시 OO로 OO길 100, 1층 붉은색 문의 건물입니다."
-                                        placeholder="소품샵 주소를 입력하세요">
-                                </div>
+                                        <div class="shop__map">
+                                            <div id="map" style="width:420px;height:230px;margin-left: 50px;"></div>
+                                            <script type="text/javascript"
+                                                src="//dapi.kakao.com/v2/maps/sdk.js?appkey=9d3637cebd20b21457c4e0c648d59d32">
+                                            </script>
+                                            <script>
+                                            var container = document.getElementById('map');
+                                            var options = {
+                                                center: new kakao.maps.LatLng(33.450701, 126.570667),
+                                                level: 3
+                                            };
 
-                                <div class="shop__map">
-                                    <div id="map" style="width:420px;height:230px;margin-left: 50px;"></div>
-                                    <script type="text/javascript"
-                                        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=9d3637cebd20b21457c4e0c648d59d32">
-                                    </script>
-                                    <script>
-                                    var container = document.getElementById('map');
-                                    var options = {
-                                        center: new kakao.maps.LatLng(33.450701, 126.570667),
-                                        level: 3
-                                    };
-
-                                    var map = new kakao.maps.Map(container, options);
-                                    </script>
+                                            var map = new kakao.maps.Map(container, options);
+                                            </script>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <!-- <div class="post__share ir">공유</div>
+                                <!-- <div class="post__share ir">공유</div>
                         <div class="post__heart ir">좋아요</div> -->
-                        <div class="tag">
-                            <span class="tag__title">키워드 태그</span>
-                            <input type="text" id="tag" value="제로웨이스트" placeholder="태그를 입력해주세요">
-                            <input type="text" id="tag" value="패브릭" placeholder="태그를 입력해주세요">
+                                <div class="tag">
+                                    <span class="tag__title">키워드 태그</span>
+                                    <input type="text" id="tag" value=<?=$info['shopTag']?> placeholder="태그를 입력해주세요">
+                                    <!-- <input type="text" id="tag" value="패브릭" placeholder="태그를 입력해주세요">
                             <input type="text" id="tag" value="코지" placeholder="태그를 입력해주세요">
                             <input type="text" id="tag" value="에코프렌들리" placeholder="태그를 입력해주세요">
-                            <input type="text" id="tag" value="인테리어" placeholder="태그를 입력해주세요">
+                            <input type="text" id="tag" value="인테리어" placeholder="태그를 입력해주세요"> -->
 
-                        </div>
-                    </div>
-                    <div class="container">
-                        <div class="post__btn">
-                            <div class="post__btn__left">
-                                <a href="sopumList.html"><button type="submit" class="btn1">목록으로</button></a>
+                                </div>
                             </div>
-                            <div class="post__btn__right">
-                                <a href="sopumListDelete.html"><button type="submit" class="btn2">삭제하기</button></a>
-                                <a href="sopumList.html"><button type="submit" class="btn3">수정완료</button></a>
+                            <div class="container">
+                                <div class="post__btn">
+                                    <div class="post__btn__left">
+                                        <a href="sopumList.php" class="btn1">목록으로</a>
+                                    </div>
+                                    <div class="post__btn__right">
+                                        <a href="sopumListDelete.php?shopListID=<?=$shopListID ?>" class="btn2">삭제하기</a>
+                                        <button type="submit" class="btn3">수정완료</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                </form>
+                </fieldset>
                 <!-- //소품샵 게시판 -->
 
 
---
+++ sabbun/sopumList/sopumListModifySave.php
@@ -0,0 +1,89 @@
+<?php 
+    include "../connect/connect.php";
+    include "../connect/session.php";
+    include "../connect/sessionCheck.php";
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
+    $youPhone = $_POST['youPhone'];
+    $youPass = $_POST['youPass'];
+    $myMemberID = $_SESSION['myMemberID'];
+    $youEmail = $_SESSION['youEmail'];
+
+    // 데이터 가져오기 --> 유효성 검사 --> 데이터 조회 --> 로그인
+    $sql = "SELECT myMemberID, youName, youNickName, youEmail, youPass, youPhone, blogImgFile, blogImgSize FROM myBMember WHERE myMemberID = {$myMemberID}";
+    $result = $connect -> query($sql);
+    $info = $result -> fetch_array(MYSQLI_ASSOC);
+
+
+    function msg($alert){
+        echo "<p class='alert'>{$alert}</p>";
+    }
+
+   // 비밀번호 검사
+
+   if($info['youPass'] == $youPass){
+        echo "비밀번호 일치";
+        $sql = "UPDATE myBMember SET youPhone = '{$youPhone}' WHERE myMemberID = '{$myMemberID}'";
+        $connect -> query($sql);  
+
+    }else{
+        echo "<script>alert('비밀번호가 틀렸습니다.'); history.back(1)</script>";
+        exit;
+    }
+
+  
+    $blogImgFile = $_FILES['blogFile'];
+    $blogImgSize = $_FILES['blogFile']['size'];
+    $blogImgType = $_FILES['blogFile']['type'];
+    $blogImgName = $_FILES['blogFile']['name'];
+    $blogImgTmp = $_FILES['blogFile']['tmp_name'];
+
+
+    //이미지 파일명 확인
+    if($blogImgType){
+        $fileTypeExtension = explode("/", $blogImgType);
+        $fileType = $fileTypeExtension[0];      //image
+        $fileExtension = $fileTypeExtension[1]; //png
+        //이미지 타입 확인
+        if($fileType == "image"){
+            if($fileExtension == "jpg" || $fileExtension == "jpeg" || $fileExtension == "png" || $fileExtension == "gif"){
+                $blogImgDir = "../html/assets/img/profile/";
+                $blogImgName = "Img_".time().rand(1,99999)."."."{$fileExtension}";
+                //echo "이미지 파일이 맞네요!";
+                $sql = "UPDATE myBMember SET blogImgFile = '{$blogImgName}', blogImgSize = '{$blogImgSize}' WHERE myMemberID = '{$myMemberID}'";
+                $result2 = $connect -> query($sql);
+                $result2 = move_uploaded_file($blogImgTmp, $blogImgDir.$blogImgName);
+
+            } else {
+                echo "<script>alert('지원하는 이미지 파일이 아닙니다.'); history.back(1)</script>";
+                exit;
+            }
+        }
+    }else {
+        echo "<p>프로필 사진을 첨부하지 않았습니다. <br> 마이 페이지에서 추가 해주세요!</p>";
+        
+    }
+
+    //이미지 사이즈 확인
+    if($blogImgSize > 1000000){
+        echo "<script>alert('이미지 용량이 1메가를 초과했습니다.'); history.back(1)</script>";
+        exit;
+    }else{
+        "<script>alert('회원 정보가 수정됐습니다.');</script>";
+        Header("Location: ../mypage/mypage.php");
+    }
+
+   
+    
+
+
+?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.10/dist/sweetalert2.min.css">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.10/dist/sweetalert2.min.js"></script>
+
+<script>
+
+
+
+</script>
