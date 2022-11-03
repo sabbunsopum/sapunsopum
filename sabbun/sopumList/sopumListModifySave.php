d41b6a113ef452119883fe9052a0086736f7a654
sabbunsopum <115064300+sabbunsopum@users.noreply.github.com>
2022-11-03 16:11:54 +0900

    asd
    asd

--- sabbun/sopumList/sopumListModifySave.php
+++ sabbun/sopumList/sopumListModifySave.php
@@ -5,13 +5,20 @@
     echo "<pre>";
     var_dump($_POST);
     echo "</pre>";
-    $youPhone = $_POST['youPhone'];
-    $youPass = $_POST['youPass'];
+    $shopName = $_POST['shopName'];
+    $shopListContents = $_POST['shopListContents'];
+    $shopHours = $_POST['shopHours'];
+    $shopNum = $_POST['shopNum'];
+    $goodsList = $_POST['goodsList'];
+    $shopAdress = $_POST['shopAdress'];
+    $shopTag = $_POST['shopTag'];
+
+
     $myMemberID = $_SESSION['myMemberID'];
     $youEmail = $_SESSION['youEmail'];
 
     // 데이터 가져오기 --> 유효성 검사 --> 데이터 조회 --> 로그인
-    $sql = "SELECT myMemberID, youName, youNickName, youEmail, youPass, youPhone, blogImgFile, blogImgSize FROM myBMember WHERE myMemberID = {$myMemberID}";
+    $sql = "UPDATE sopumShopList SET youPhone = '{$youPhone}' WHERE myMemberID = '{$myMemberID}'";
     $result = $connect -> query($sql);
     $info = $result -> fetch_array(MYSQLI_ASSOC);
 
@@ -33,27 +40,27 @@
     }
 
   
-    $blogImgFile = $_FILES['blogFile'];
-    $blogImgSize = $_FILES['blogFile']['size'];
-    $blogImgType = $_FILES['blogFile']['type'];
-    $blogImgName = $_FILES['blogFile']['name'];
-    $blogImgTmp = $_FILES['blogFile']['tmp_name'];
+    $sopumImgFile = $_FILES['shopProfile'];
+    $sopumImgSize = $_FILES['shopProfile']['size'];
+    $sopumImgType = $_FILES['shopProfile']['type'];
+    $sopumImgName = $_FILES['shopProfile']['name'];
+    $sopumImgTmp = $_FILES['shopProfile']['tmp_name'];
 
 
     //이미지 파일명 확인
-    if($blogImgType){
-        $fileTypeExtension = explode("/", $blogImgType);
+    if($sopumImgType){
+        $fileTypeExtension = explode("/", $sopumImgType);
         $fileType = $fileTypeExtension[0];      //image
         $fileExtension = $fileTypeExtension[1]; //png
         //이미지 타입 확인
         if($fileType == "image"){
             if($fileExtension == "jpg" || $fileExtension == "jpeg" || $fileExtension == "png" || $fileExtension == "gif"){
-                $blogImgDir = "../html/assets/img/profile/";
-                $blogImgName = "Img_".time().rand(1,99999)."."."{$fileExtension}";
+                $sopumImgDir = "../html/assets/img/profile/";
+                $sopumImgName = "Img_".time().rand(1,99999)."."."{$fileExtension}";
                 //echo "이미지 파일이 맞네요!";
-                $sql = "UPDATE myBMember SET blogImgFile = '{$blogImgName}', blogImgSize = '{$blogImgSize}' WHERE myMemberID = '{$myMemberID}'";
+                $sql = "UPDATE myBMember SET sopumImgFile = '{$sopumImgName}', sopumImgSize = '{$sopumImgSize}' WHERE myMemberID = '{$myMemberID}'";
                 $result2 = $connect -> query($sql);
-                $result2 = move_uploaded_file($blogImgTmp, $blogImgDir.$blogImgName);
+                $result2 = move_uploaded_file($sopumImgTmp, $sopumImgDir.$sopumImgName);
 
             } else {
                 echo "<script>alert('지원하는 이미지 파일이 아닙니다.'); history.back(1)</script>";
@@ -66,12 +73,12 @@
     }
 
     //이미지 사이즈 확인
-    if($blogImgSize > 1000000){
+    if($sopumImgSize > 1000000){
         echo "<script>alert('이미지 용량이 1메가를 초과했습니다.'); history.back(1)</script>";
         exit;
     }else{
-        "<script>alert('회원 정보가 수정됐습니다.');</script>";
-        Header("Location: ../mypage/mypage.php");
+        "<script>alert('소품샵 정보가 수정됐습니다.');</script>";
+        Header("Location: sopumList.php");
     }
 
    
