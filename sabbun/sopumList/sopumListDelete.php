3051a9278749005015f2a6fbbae53f089bb5eca1
sabbunsopum <115064300+sabbunsopum@users.noreply.github.com>
2022-11-03 19:01:12 +0900

    asd
    asd

--- sabbun/sopumList/sopumListDelete.php
+++ sabbun/sopumList/sopumListDelete.php
@@ -21,12 +21,15 @@
     if($memberInfo['myMemberID'] === $myMemberID && $listInfo['myMemberID'] === $myMemberID ){
         $sql = "DELETE FROM sopumShopList WHERE shopListID = {$shopListID}";
         $connect -> query($sql);
-        echo "<script>alert('삭제했습니다.')</script>";
+        echo "<script>alert('삭제 했습니다.'); history.back(1)</script>";
+        Header("Location: sopumList.php");
+        
     } else {
-        echo "<script>alert('내가 작성한 글이 아닙니다. 다시 한번 확인해주세요!')</script>";
+        echo "<script>alert('내가 작성한 글이 아닙니다.'); history.back(1)</script>";
+        exit;
     } 
     
-    Header("Location: sopumList.php");
+    
 ?>
 <script>
 </script>
