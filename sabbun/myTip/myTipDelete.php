3bdabb8d10b4bddddac4bc3b02addcebded76df9
sabbunsopum <115064300+sabbunsopum@users.noreply.github.com>
2022-11-03 18:12:48 +0900

    delete
    123

--- sabbun/myTip/myTipDelete.php
+++ sabbun/myTip/myTipDelete.php
@@ -1,12 +1,35 @@
 <?php
     include "../connect/connect.php";
     include "../connect/session.php";
-    include "../connect/sessionCheck.php";
+    //include "../connect/sessionCheck.php";
     
-    $myMemberID = $_POST['myMemberID'];
+    $myTipID = $_GET['myTipID'];
+    $myMemberID = $_SESSION['myMemberID'];
+    
+    $tipSql = "SELECT * FROM myTip";
+    $tipResult = $connect -> query($tipSql);
+    $tipInfo = $tipResult -> fetch_array(MYSQLI_ASSOC);
 
-    $sql = "DELETE FROM myTip WHERE myMemberID = {$myMemberID}";
-    $result = $connect -> query($sql);
+
+
+    if($tipInfo['myMemberID'] === $myMemberID){
+        $sql = "DELETE FROM myTip WHERE myTipID = {$myTipID}";
+        $result = $connect -> query($sql);
+        echo '<script type="text/javascript">'; 
+        echo 'alert("삭제 했습니다.");'; 
+        echo 'window.location.href = "myTip.php";';
+        echo '</script>';
+    } else {
+        echo '<script type="text/javascript">'; 
+        echo 'alert("내가 작성한 글이 아닙니다.");'; 
+        echo 'window.location.href = "myTip.php";';
+        echo '</script>';
+    } 
+
+   
+
     
-    echo json_encode(array("info" => $myTipID));
+    
+    //Header("Location: myTip.php");
+    //echo json_encode(array("info" => $myTipID));
 ?>
