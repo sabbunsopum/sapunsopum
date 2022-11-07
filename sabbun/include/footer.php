d58d672fed99885620f08973cecdb5b069b01109
sabbunsopum <115064300+sabbunsopum@users.noreply.github.com>
2022-11-07 15:17:35 +0900

    Update footer.php

--- sabbun/include/footer.php
+++ sabbun/include/footer.php
@@ -46,11 +46,11 @@
                 </p>
             </div>
             <div class="footer__icon">
-                <div class="footer__icon__img img1"><a href=""></a></div>
-                <div class="footer__icon__img img2"><a href=""></a></div>
-                <div class="footer__icon__img img3"><a href=""></a></div>
-                <div class="footer__icon__img img4"><a href=""></a></div>
-                <div class="footer__icon__img img5"><a href=""></a></div>
+                <div class="footer__icon__img img1"><a href="#"></a></div>
+                <div class="footer__icon__img img2"><a href="#"></a></div>
+                <div class="footer__icon__img img3"><a href="#"></a></div>
+                <div class="footer__icon__img img4"><a href="#"></a></div>
+                <div class="footer__icon__img img5"><a href="#"></a></div>
             </div>
         </div>
 
