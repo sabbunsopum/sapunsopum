20e62ec1f386783bee001416a263269de49cce38
sabbunsopum <115064300+sabbunsopum@users.noreply.github.com>
2022-11-07 15:47:57 +0900

    asd
    asd

--- sabbun/sopumList/sopumListModifySave.php
+++ sabbun/sopumList/sopumListModifySave.php
@@ -51,32 +51,45 @@
                     $sopumImgName = "Img_".time().rand(1,99999)."."."{$fileExtension}";
                     //echo "이미지 파일이 맞네요!";
                     $sql = "UPDATE sopumShopList SET shopImgFile = '{$sopumImgName}', shopImgSize = '{$sopumImgSize}' WHERE shopListID = '{$shopListID}'";
-                    $result2 = $connect -> query($sql);
-                    $result2 = move_uploaded_file($sopumImgTmp, $sopumImgDir.$sopumImgName);
+                    
     
                 } else {
-                    echo "<script>alert('지원하는 이미지 파일이 아닙니다.'); history.back(1)</script>";
+                    echo '<script type="text/javascript">'; 
+                    echo 'alert("지원하는 이미지 파일이 아닙니다.");'; 
+                    echo 'history.back(1)';
+                    echo '</script>';
                     exit;
                 }
             }
         }else {
-            echo "<script>alert('사진을 첨부하지 않았습니다.'); history.back(1)</script>";
+            echo '<script type="text/javascript">'; 
+            echo 'alert("사진을 첨부하지 않았습니다.");'; 
+            echo 'history.back(1)';
+            echo '</script>';
             exit;
         }
  
     
     }else{
-        echo "<script>alert('내가 작성한 글이 아닙니다.'); history.back(1)</script>";
+        echo '<script type="text/javascript">'; 
+        echo 'alert("내가 작성한 글이 아닙니다..");'; 
+        echo 'history.back(1)';
+        echo '</script>';
         exit;
     }
     
 
     //이미지 사이즈 확인
     if($sopumImgSize > 1000000){
-        echo "<script>alert('이미지 용량이 1메가를 초과했습니다.'); history.back(1)</script>";
+        echo '<script type="text/javascript">'; 
+        echo 'alert("용량이 1메가를 초과했습니다.");'; 
+        echo 'history.back(1)';
+        echo '</script>';
         exit;
     }else{
         "<script>alert('소품샵 정보가 수정됐습니다.');</script>";
+        $result2 = $connect -> query($sql);
+        $result2 = move_uploaded_file($sopumImgTmp, $sopumImgDir.$sopumImgName);
         Header("Location: sopumList.php");
     }
 
